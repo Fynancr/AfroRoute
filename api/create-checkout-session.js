79860817f76835f221b0aba1403dea4cab899253
Fynancr Inc. <213638059+Fynancr@users.noreply.github.com>
2026-04-25 23:40:44 +0200

    Fix syntax error in create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -2,7 +2,8 @@ const Stripe = require('stripe');
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error:
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
